f4c799452e22f99cd4f7b65b99144fd7e3f97056
YasuoSEKITANYA <YasuoSEKITANI@outlook.jp>
2019-11-20 11:25:24 +0900

    testBrunch

--- init.el
+++ init.el
@@ -1,11 +1,11 @@
-;; package.el
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+﻿;; package.el
+;(require 'package)
+;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+;(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 
-(package-initialize)
+;(package-initialize)
 
 
 
@@ -58,67 +58,67 @@
 (define-key ctl-w-map (kbd "h") 'windmove-right)
 
 
-(custom-set-variables
+;(custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (mozc yatex))))
-(custom-set-faces
+; '(package-selected-packages (quote (mozc yatex))))
+;(custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+; )
 
 
 ;; yatex
-(require 'yatex)
-(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
-(setq auto-mode-alist
-      (append '(("\\.tex$" . yatex-mode)
-                ("\\.ltx$" . yatex-mode)
-                ("\\.cls$" . yatex-mode)
-                ("\\.sty$" . yatex-mode)
-                ("\\.clo$" . yatex-mode)
-                ("\\.bbl$" . yatex-mode)) auto-mode-alist))
-(setq YaTeX-inhibit-prefix-letter t)
-(setq YaTeX-kanji-code nil)
-(setq YaTeX-latex-message-code 'utf-8)
-(setq YaTeX-use-LaTeX2e t)
-(setq YaTeX-use-AMS-LaTeX t)
-(setq YaTeX-dvi2-command-ext-alist
-      '(("TeXworks\\|texworks\\|texstudio\\|mupdf\\|SumatraPDF\\|Preview\\|Skim\\|TeXShop\\|evince\\|atril\\|xreader\\|okular\\|zathura\\|qpdfview\\|Firefox\\|firefox\\|chrome\\|chromium\\|MicrosoftEdge\\|microsoft-edge\\|Adobe\\|Acrobat\\|AcroRd32\\|acroread\\|pdfopen\\|xdg-open\\|open\\|start" . ".pdf")))
-(setq tex-command "platex")
-(setq bibtex-command "pbibtex")
-(setq tex-pdfview-command "rundll32 shell32,ShellExec_RunDLL SumatraPDF -reuse-instance")
+;(require 'yatex)
+;(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+;(setq auto-mode-alist
+;     (append '(("\\.tex$" . yatex-mode)
+;                ("\\.ltx$" . yatex-mode)
+;                ("\\.cls$" . yatex-mode)
+;                ("\\.sty$" . yatex-mode)
+;                ("\\.clo$" . yatex-mode)
+;                ("\\.bbl$" . yatex-mode)) auto-mode-alist))
+;(setq YaTeX-inhibit-prefix-letter t)
+;(setq YaTeX-kanji-code nil)
+;(setq YaTeX-latex-message-code 'utf-8)
+;(setq YaTeX-use-LaTeX2e t)
+;(setq YaTeX-use-AMS-LaTeX t)
+;(setq YaTeX-dvi2-command-ext-alist
+;      '(("TeXworks\\|texworks\\|texstudio\\|mupdf\\|SumatraPDF\\|Preview\\|Skim\\|TeXShop\\|evince\\|atril\\|xreader\\|okular\\|zathura\\|qpdfview\\|Firefox\\|firefox\\|chrome\\|chromium\\|MicrosoftEdge\\|microsoft-edge\\|Adobe\\|Acrobat\\|AcroRd32\\|acroread\\|pdfopen\\|xdg-open\\|open\\|start" . ".pdf")))
+;(setq tex-command "platex")
+;(setq bibtex-command "pbibtex")
+;(setq tex-pdfview-command "rundll32 shell32,ShellExec_RunDLL SumatraPDF -reuse-instance")
 (setq dviprint-command-format "powershell -Command \"& {$r = Write-Output %s;$p = [System.String]::Concat('\"\"\"',[System.IO.Path]::GetFileNameWithoutExtension($r),'.pdf','\"\"\"');Start-Process AcroRd32 -ArgumentList ($p)}\"")
-(defun fwdsumatrapdf-forward-search ()
-  (interactive)
-  (progn
-    (process-query-on-exit-flag
-     (start-process
-      "fwdsumatrapdf"
-      nil
-      "fwdsumatrapdf"
-      (expand-file-name
-       (concat (file-name-sans-extension (or YaTeX-parent-file
-                                             (save-excursion
-                                               (YaTeX-visit-main t)
-                                               buffer-file-name)))
-               ".pdf"))
-      (buffer-name)
-      (number-to-string (save-restriction
-                          (widen)
-                          (count-lines (point-min) (point))))))))
+;(defun fwdsumatrapdf-forward-search ()
+; (interactive)
+;  (progn
+;    (process-query-on-exit-flag
+;     (start-process
+;      "fwdsumatrapdf"
+;      nil
+;      "fwdsumatrapdf"
+;      (expand-file-name
+;       (concat (file-name-sans-extension (or YaTeX-parent-file
+;                                             (save-excursion
+;                                               (YaTeX-visit-main t)
+;                                               buffer-file-name)))
+;               ".pdf"))
+;      (buffer-name)
+;      (number-to-string (save-restriction
+;                          (widen)
+;                          (count-lines (point-min) (point))))))))
 
-(add-hook 'yatex-mode-hook
-          '(lambda ()
-             (define-key YaTeX-mode-map (kbd "C-c f") 'fwdsumatrapdf-forward-search)))
+;(add-hook 'yatex-mode-hook
+;          '(lambda ()
+;             (define-key YaTeX-mode-map (kbd "C-c f") 'fwdsumatrapdf-forward-search)))
 
-(add-hook 'yatex-mode-hook
-          '(lambda ()
-             (auto-fill-mode -1)))
+;(add-hook 'yatex-mode-hook
+;          '(lambda ()
+;             (auto-fill-mode -1)))
 
 ;;
 ;; RefTeX with YaTeX
